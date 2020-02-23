5b65ae0b41504543f4b17e8353d62eadf1ddbb57
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2020-02-23 17:22:36 +0100

    feat(dependencies): add new dependencies to change the builds
    add compression webpack plugin to reduce the final bundle of the app
    add imagemin webpack plugin to compress the images during the build

--- scripts/webpack/custom-webpack-build.config.js
+++ scripts/webpack/custom-webpack-build.config.js
@@ -1,35 +1,8 @@
-const MOMENT_LOCALES_WEBPACK_PLUGIN = require('moment-locales-webpack-plugin');
-const MOMENT_TIMEZONE_DATA_WEBPACK_PLUGIN = require('moment-timezone-data-webpack-plugin');
 const IMAGEMIN_WEBPACK_PLUGIN = require('imagemin-webpack-plugin').default;
 const COMPRESSION_WEBPACK_PLUGIN = require('compression-webpack-plugin');
 
-const currentYear = new Date().getFullYear();
-
 module.exports = {
   plugins: [
-
-    /**
-     * @description
-     * Optimize the moment bundle size
-     * Remove all extra locales except some specifics ones
-     */
-    new MOMENT_LOCALES_WEBPACK_PLUGIN({
-      localesToKeep: [
-        'en',
-        'fr'
-      ]
-    }),
-
-    /**
-     * @description
-     * Optimize the moment timezone bundle size
-     * Remove some dates which should never be set
-     * Be careful though
-     */
-    new MOMENT_TIMEZONE_DATA_WEBPACK_PLUGIN({
-      startYear: currentYear - 10,
-      endYear: currentYear + 50
-    }),
 
     /**
      * @description
--- scripts/webpack/custom-webpack-test.config.js
+++ scripts/webpack/custom-webpack-test.config.js
@@ -1,32 +1,3 @@
-const MOMENT_LOCALES_WEBPACK_PLUGIN = require('moment-locales-webpack-plugin');
-const MOMENT_TIMEZONE_DATA_WEBPACK_PLUGIN = require('moment-timezone-data-webpack-plugin');
-
-const currentYear = new Date().getFullYear();
-
 module.exports = {
-  plugins: [
-
-    /**
-     * @description
-     * Optimize the moment bundle size
-     * Remove all extra locales except some specifics ones
-     */
-    new MOMENT_LOCALES_WEBPACK_PLUGIN({
-      localesToKeep: [
-        'en',
-        'fr'
-      ]
-    }),
-
-    /**
-     * @description
-     * Optimize the moment timezone bundle size
-     * Remove some dates which should never be set
-     * Be careful though
-     */
-    new MOMENT_TIMEZONE_DATA_WEBPACK_PLUGIN({
-      startYear: currentYear - 10,
-      endYear: currentYear + 50
-    })
-  ]
+  plugins: []
 };
