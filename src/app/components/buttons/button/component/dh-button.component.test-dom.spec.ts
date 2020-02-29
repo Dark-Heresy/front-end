ee58bcd5baa8da5a9b30dfc54d815177721276d2
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2020-02-29 09:10:56 +0100

    test(button-component): add more coverage

--- src/app/components/buttons/button/component/dh-button.component.test-dom.spec.ts
+++ src/app/components/buttons/button/component/dh-button.component.test-dom.spec.ts
@@ -1,6 +1,8 @@
 import { DhButtonComponent } from '@dh/components/buttons/button/component/dh-button.component';
 import { DhButtonSizeEnum } from '@dh/components/buttons/button/enums/dh-button-size.enum';
 import { DhButtonTypeEnum } from '@dh/components/buttons/button/enums/dh-button-type-enum';
+import { IDhButtonClickEvent } from '@dh/components/buttons/button/interfaces/dh-button-click-event';
+import { DhDisabledModule } from '@dh/directives/disabled/dh-disabled.module';
 import {
   createComponentFactory,
   Spectator
@@ -10,7 +12,10 @@ import { cleanStylesFromDom } from '@test/test';
 describe('DhButtonComponent:TestDOM', () => {
   const componentRootClass = '.dh-button';
   const createComponent = createComponentFactory({
-    component: DhButtonComponent
+    component: DhButtonComponent,
+    imports: [
+      DhDisabledModule
+    ]
   });
   let spectator: Spectator<DhButtonComponent>;
 
@@ -18,12 +23,52 @@ describe('DhButtonComponent:TestDOM', () => {
     spectator = createComponent();
   });
 
-  it('should have a medium size class', () => {
+  it('should have a medium size by default', () => {
     expect(spectator.query(componentRootClass)).toHaveClass(DhButtonSizeEnum.MEDIUM);
   });
 
-  it('should have a primary type class', () => {
+  it('should have a primary type by default', () => {
     expect(spectator.query(componentRootClass)).toHaveClass(DhButtonTypeEnum.PRIMARY);
+  });
+
+  it('should not be disabled by default', () => {
+    expect(spectator.query(componentRootClass)).not.toHaveAttribute('disabled');
+    expect(spectator.query(componentRootClass)).not.toHaveAttribute('dh-button-disabled');
+  });
+
+  it('should be focusable by default', () => {
+    expect(spectator.query(componentRootClass)).toHaveAttribute('tabindex', '0');
+  });
+
+  describe('when the button is clicked', () => {
+    let output: IDhButtonClickEvent;
+
+    describe('when the button is disabled', () => {
+      beforeEach(() => {
+        spectator.component.isDisabled = true;
+      });
+
+      it('should not notify the click', () => {
+        spectator.output('clickEvent').subscribe((result: any) => (output = result));
+
+        spectator.click(spectator.query(componentRootClass) as Element);
+        expect(output).toBeUndefined();
+      });
+    });
+
+    describe('when the button is not disabled', () => {
+      beforeEach(() => {
+        spectator.component.isDisabled = false;
+      });
+
+      it('should notify the click', () => {
+        spectator.output('clickEvent').subscribe((result: any) => (output = result));
+
+        spectator.click(spectator.query(componentRootClass) as Element);
+        expect(output).toBeDefined();
+        expect(output.mouseEvent).toEqual(jasmine.any(MouseEvent));
+      });
+    });
   });
 
   afterAll(() => {
